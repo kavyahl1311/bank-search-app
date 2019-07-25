9aee2e3938ab6d2a4b1b77447563e1ad34de9cc7
kavyahl <kavyahl1234@gmail.com>
2019-07-25 23:23:52 +0530

    added styling to table and export option

--- src/components/BankListView.js
+++ src/components/BankListView.js
@@ -8,18 +8,52 @@ class BankListView extends Component {
             <MaterialTable
                 title="Bank Search App"
                 columns={[
-                    { title: 'IFSC', field: 'ifsc' },
-                    { title: 'BANK ID', field: 'bank_id', type: 'numeric' },
-                    { title: 'BRANCH', field: 'branch' },
-                    { title: 'ADDRESS', field: 'address', },
-                    { title: 'CITY', field: 'city', },
-                    { title: 'DISTRICT', field: 'district', },
-                    { title: 'STATE', field: 'state', },
-                    { title: 'BANK NAME', field: 'bank_name', },
+                    {
+                        title: 'BANK NAME', field: 'bank_name', headerStyle: {
+                            backgroundColor: '#039be5',
+                        },
+                    },
+                    {
+                        title: 'IFSC', field: 'ifsc', headerStyle: {
+                            backgroundColor: '#039be5',
+                        }
+                    },
+                    {
+                        title: 'BANK ID', field: 'bank_id', type: 'numeric', headerStyle: {
+                            backgroundColor: '#039be5',
+                        }
+                    },
+                    {
+                        title: 'BRANCH', field: 'branch', headerStyle: {
+                            backgroundColor: '#039be5',
+                        }
+                    },
+                    {
+                        title: 'ADDRESS', field: 'address', headerStyle: {
+                            backgroundColor: '#039be5',
+                        }
+                    },
+                    {
+                        title: 'CITY', field: 'city', headerStyle: {
+                            backgroundColor: '#039be5',
+                        }
+                    },
+                    {
+                        title: 'DISTRICT', field: 'district', headerStyle: {
+                            backgroundColor: '#039be5',
+                        }
+                    },
+                    {
+                        title: 'STATE', field: 'state', headerStyle: {
+                            backgroundColor: '#039be5',
+                        }
+                    }
                 ]}
                 data={this.props.data}
                 options={{
-                    search: true
+                    search: true,
+                    exportButton: true,
+                    grouping: true
                 }}
             />
         )
